e41dc9a437881872892a3e98461f94e8f7995410
Sandeep Dhamala <141273955+sandeep0600@users.noreply.github.com>
2024-05-25 16:23:50 +0545

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,8 +1,7 @@
 const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    baseUrl: 'https://your-cloudflare-worker-url',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
-}
+};
 
 export default apiConfig;
