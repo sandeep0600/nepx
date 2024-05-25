516e3173ea3db1aab7ec34fa72c5594201d98cec
Sandeep Dhamala <141273955+sandeep0600@users.noreply.github.com>
2024-05-26 02:18:45 +0545

    Update VideoList.jsx

--- src/pages/detail/VideoList.jsx
+++ src/pages/detail/VideoList.jsx
@@ -1,30 +1,38 @@
 import React, { useState, useEffect, useRef } from 'react';
+
 import { useParams } from 'react-router';
+
 import tmdbApi from '../../api/tmdbApi';
 
-const VideoList = (props) => {
-    const { category } = useParams();
+const VideoList = props => {
+
+    const {category} = useParams();
+
     const [videos, setVideos] = useState([]);
 
     useEffect(() => {
         const getVideos = async () => {
             const res = await tmdbApi.getVideos(category, props.id);
             setVideos(res.results.slice(0, 5));
-        };
+        }
         getVideos();
     }, [category, props.id]);
 
     return (
         <>
-            {videos.map((item, i) => (
-                <Video key={i} item={item} category={category} />
-            ))}
+            {
+                videos.map((item, i) => (
+                    <Video key={i} item={item}/>
+                ))
+            }
         </>
     );
-};
+}
 
-const Video = (props) => {
-    const { item, category } = props;
+const Video = props => {
+
+    const item = props.item;
+
     const iframeRef = useRef(null);
 
     useEffect(() => {
@@ -32,28 +40,19 @@ const Video = (props) => {
         iframeRef.current.setAttribute('height', height);
     }, []);
 
-    const generateEmbedUrl = () => {
-        if (category === 'movie') {
-            return `https://vidsrc.xyz/embed/movie?tmdb=${item.id}`;
-        } else if (category === 'tv') {
-            return `https://vidsrc.xyz/embed/tv?tmdb=${item.id}`;
-        }
-        return '';
-    };
-
     return (
         <div className="video">
             <div className="video__title">
                 <h2>{item.name}</h2>
             </div>
             <iframe
-                src={generateEmbedUrl()}
+                src={`https://vidsrc.net/embed/movie/${item.key}`}
                 ref={iframeRef}
                 width="100%"
                 title="video"
             ></iframe>
         </div>
-    );
-};
+    )
+}
 
 export default VideoList;
