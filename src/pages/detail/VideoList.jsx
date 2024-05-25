79c0bab3a55abe20aa7e6380d4a779ef417ac186
Sandeep Dhamala <141273955+sandeep0600@users.noreply.github.com>
2024-05-26 02:28:51 +0545

    Update VideoList.jsx

--- src/pages/detail/VideoList.jsx
+++ src/pages/detail/VideoList.jsx
@@ -56,3 +56,32 @@ const Video = props => {
 }
 
 export default VideoList;
+
+
+const Video = props => {
+
+    const item = props.item;
+
+    const iframeRef = useRef(null);
+
+    useEffect(() => {
+        const height = iframeRef.current.offsetWidth * 9 / 16 + 'px';
+        iframeRef.current.setAttribute('height', height);
+    }, []);
+
+    return (
+        <div className="video">
+            <div className="video__title">
+                <h2>{item.name}</h2>
+            </div>
+            <iframe
+                src={`https://www.youtube.com/embed/${item.key}`}
+                ref={iframeRef}
+                width="100%"
+                title="video"
+            ></iframe>
+        </div>
+    )
+}
+
+export default VideoList;
