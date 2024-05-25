8833d2511e0af7ae705be8155e3fd98b24ff1a88
Sandeep Dhamala <141273955+sandeep0600@users.noreply.github.com>
2024-05-25 16:48:19 +0545

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,7 @@
 const apiConfig = {
     baseUrl: 'https://imdb.hriks.com',
-    apiKey: '', // Add your API key if needed
+    apiKey: '73bbcb846da6fff8be578b7da63fadd2',
+    apiReadAccessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3M2JiY2I4NDZkYTZmZmY4YmU1NzhiN2RhNjNmYWRkMiIsInN1YiI6IjY2NTFiZDYxMDJmMjk0Zjc1MTI2ZjE1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.24VOm8isrbIHqYZ9XwPkfmc3sGam754W-k2BQFKA4kg',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 };
