3f71cda50998a97ad38e562abed0c680afd97326
Sandeep Dhamala <141273955+sandeep0600@users.noreply.github.com>
2024-05-25 16:49:49 +0545

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-    baseUrl: 'https://imdb.hriks.com',
+    baseUrl: 'https://api.themoviedb.org/3',
     apiKey: '73bbcb846da6fff8be578b7da63fadd2',
     apiReadAccessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3M2JiY2I4NDZkYTZmZmY4YmU1NzhiN2RhNjNmYWRkMiIsInN1YiI6IjY2NTFiZDYxMDJmMjk0Zjc1MTI2ZjE1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.24VOm8isrbIHqYZ9XwPkfmc3sGam754W-k2BQFKA4kg',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
